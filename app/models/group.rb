ee3eb58090318b780b33a544abcceed07e0eb217
T S <YenTheFirst@gmail.com>
2011-03-28 16:10:33 -0700

    abstracted out phone # validation a bit. still some refactoring to do (massage_number is duplicated in both group and student)

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,15 @@ class Group < ActiveRecord::Base
   belongs_to :user
   has_many :memberships, :dependent=>:destroy
   has_many :students, :through=> :memberships
+  validates_format_of :phone_number, :with=>PHONE_FORMAT, :message=>PHONE_FORMAT_MESSAGE
+  before_validation :massage_number
+  
+  private
+  def massage_number
+    if !(phone_number =~ PHONE_FORMAT)
+      if phone_number && (nums=phone_number.scan(/\d/)).length==10
+        self.phone_number="(%s%s%s) %s%s%s-%s%s%s%s"%nums #is there a better way to write this?
+      end
+    end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,8 @@
 class Student < ActiveRecord::Base
   has_many :memberships, :dependent=>:destroy
   has_many :groups, :through=> :memberships
-  PHONE_REGEX=/^\(\d{3}\) \d{3}-\d{4}$/
   #for the moment, we expect standard, US-style 10 digit area code/number. we assume a country code of 1.
-  validates_format_of :phone_number, :with=>PHONE_REGEX, :message=>"phone number must be 10 digits, and of the form '(xxx) xxx-xxxx'"
+  validates_format_of :phone_number, :with=>PHONE_FORMAT, :message=>PHONE_FORMAT_MESSAGE
   
   before_validation :massage_number
   
@@ -11,8 +10,8 @@ class Student < ActiveRecord::Base
   
   #maybe this should be public, so javascript can call out to it, instead of reimplimenting the same logic in JS
   def massage_number
-    if !(phone_number =~ PHONE_REGEX)
-      if (nums=phone_number.scan(/\d/)).length==10
+    if !(phone_number =~ PHONE_FORMAT)
+      if phone_number && (nums=phone_number.scan(/\d/)).length==10
         self.phone_number="(%s%s%s) %s%s%s-%s%s%s%s"%nums #is there a better way to write this?
       end
     end

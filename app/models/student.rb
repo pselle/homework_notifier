50f951c8178a004c49593edf1aa71c91eee40182
T S <YenTheFirst@gmail.com>
2011-03-30 16:59:57 -0700

    wrote initial spec for student

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,11 +6,13 @@ class Student < ActiveRecord::Base
   
   before_validation :massage_number
   
+  validates_uniqueness_of :phone_number
+  validates_presence_of :name
   private
   
   #maybe this should be public, so javascript can call out to it, instead of reimplimenting the same logic in JS
   def massage_number
-    if !(phone_number =~ PHONE_FORMAT)
+    if !(phone_number.to_s =~ PHONE_FORMAT)
       if phone_number && (nums=phone_number.scan(/\d/)).length==10
         self.phone_number="(%s%s%s) %s%s%s-%s%s%s%s"%nums #is there a better way to write this?
       end

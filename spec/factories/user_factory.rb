1b9655b6a5b8a742cd1ca6119650ac542442c1ed
T S <YenTheFirst@gmail.com>
2011-04-04 17:02:12 -0700

    added more comprehensive behavior tests for user, set actionmailer config to work

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,4 +1,5 @@
 Factory.define(:user) do |u|
-  u.sequence(:email) {|n| "email_#{n}@company.com"}
-  u.password "a_password"
+  u.sequence(:email) {|n| "email#{n}@testcompany.com"}
+  u.password "123456"
+  u.password_confirmation "123456"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
+#factory girl doesn't play nice with user.
 
 describe User do
   pending "is not registerable" do
     #this might be a controller test?
   end
   it "can be created manually" do
-    expect{Factory.create(:user)}.to change(User,:count).by(1)
+    expect{User.create(:email=>"bob@zombo.com")}.to change(User,:count).by(1)
   end
   it "doesn't require info before confirmation" do
-    u=Factory.create(:user)
-    u.name.should be_nil
+    u=User.new(:email=>"email@yeaaah.com")
+    u.name.should be_blank
+    u.phone_number.should be_blank
+    u.encrypted_password.should be_blank
+    u.password.should be_blank
     u.should be_valid
   end
   pending "does require profile info after confirmation" do
@@ -17,5 +21,26 @@ describe User do
   pending "can be confirmed" do
   end
   pending "disallows sign-in before confirmation" do
+  end
+  describe "their phone number, after confirmation" do
+    before(:each) do
+      @user=User.create(:email=>"hi@hi.com",:name=>"testguy",:display_name=>"j",:password=>"yeaaah",:password_confirmation=>"yeaaah")
+      @user.confirm!
+    end
+    it "may be blank" do
+      @user.phone_number=nil
+      @user.should be_valid
+    end
+    it "if present, must be of the canonical format" do
+      @user.phone_number="abc123"
+      @user.should_not be_valid
+      @user.phone_number="(123) 456-7890"
+      @user.should be_valid
+      @user.phone_number="1234567891"
+      @user.should be_valid
+    end
+  end
+  
+  describe "their password" do
   end
 end

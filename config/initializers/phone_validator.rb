7f8ebf99f7c0c0522e73138e61ccfe3f16a192ba
T S <YenTheFirst@gmail.com>
2011-04-06 21:36:27 -0700

    again with the refactoring

--- config/initializers/phone_validator.rb
+++ config/initializers/phone_validator.rb
@@ -2,16 +2,12 @@ ActiveRecord::Base.class_eval do
 
   def self.validates_phone_number(name,opts={})
     used_opts = {:with=>PhoneValidator::STORAGE_REGEX, :message=>PhoneValidator::FORMAT_ERROR_MESSAGE}.merge(opts)
-    before_validation(:massage_number) unless used_opts.delete(:massage_number) == false
+    before_validation(:massage_my_number!) unless used_opts.delete(:massage_number) == false
     validates_format_of(name, used_opts)
   end
   
-  def massage_number
-    if !(phone_number.to_s =~ PhoneValidator::STORAGE_REGEX)
-      if phone_number && (nums=phone_number.scan(/\d/)).length==10
-        self.phone_number=PhoneValidator::STORAGE_PRINTF%nums #is there a better way to write this?
-      end
-    end
-  end#of massage number
+  def massage_my_number!
+    self.phone_number=PhoneValidator::massage_number(self.phone_number)
+  end
 
 end#of class eval
--- lib/phone_validator.rb
+++ lib/phone_validator.rb
@@ -3,4 +3,12 @@ module PhoneValidator
   STORAGE_PRINTF="%s%s%s%s%s%s%s%s%s%s"
   FORMAT_ERROR_MESSAGE="phone number must be 10 digits, and of the form 'xxxxxxxxxx'"
   OUTPUT_PRINTF="(%s%s%s) %s%s%s-%s%s%s%s"
+  def self.massage_number(phone_number)
+    if !(phone_number.to_s =~ PhoneValidator::STORAGE_REGEX)
+      if phone_number && (nums=phone_number.scan(/\d/)).length==10
+        return PhoneValidator::STORAGE_PRINTF%nums #is there a better way to write this?
+      end
+    end
+    return phone_number #else.
+  end
 end

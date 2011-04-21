15206f1e3ae6d8f0c7502ac64449ddc1c41a148d
T S <YenTheFirst@gmail.com>
2011-04-21 10:48:32 -0700

    'massage my number' now respects attribute name

--- config/initializers/phone_validator.rb
+++ config/initializers/phone_validator.rb
@@ -2,12 +2,12 @@ ActiveRecord::Base.class_eval do
 
   def self.validates_phone_number(name,opts={})
     used_opts = {:with=>PhoneValidator::STORAGE_REGEX, :message=>PhoneValidator::FORMAT_ERROR_MESSAGE}.merge(opts)
-    before_validation(:massage_my_number!) unless used_opts.delete(:massage_number) == false
+    before_validation lambda {massage_my_number!(name)} unless used_opts.delete(:massage_number) == false
     validates_format_of(name, used_opts)
   end
   
-  def massage_my_number!
-    self.phone_number=PhoneValidator::massage_number(self.phone_number)
+  def massage_my_number!(name)
+    self[name]=PhoneValidator::massage_number(self[name])
   end
 
 end#of class eval

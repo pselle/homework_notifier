2b0c5bcb63c77487994d98f52e24e2a46156ff50
T S <YenTheFirst@gmail.com>
2011-05-11 18:23:47 -0700

    fix uniqueness issue with blank phone numbers

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
   #for the moment, we expect standard, US-style 10 digit area code/number. we assume a country code of 1.
 
   validates_phone_number :phone_number, :allow_blank=>true
-  validates_uniqueness_of :phone_number, :scope=>:group_id
+  validates_uniqueness_of :phone_number, :scope=>:group_id, :allow_blank=>true, :allow_nil=>true
   
   validates_presence_of :name
   
